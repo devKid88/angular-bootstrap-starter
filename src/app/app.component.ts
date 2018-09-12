8e65505444f6d462e778a29b9999db8aa5d6a392
Nemanja Marinkovic <nemanjamarinkovic88@gmail.com>
2018-09-12 11:39:52 +0200

    Setup nebular, and added basic themes, and settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-bootstrap-starter';
+
+  constructor(
+    private themeService: NbThemeService
+  ) {
+    this.themeService.changeTheme('default');
+  }
+
+  // change to dark
+  enableDarkTheme() {
+    this.themeService.changeTheme('cosmic'); // change to cosmic
+    this.themeService.changeTheme('dark'); // change to own custom theme dark
+    this.themeService.changeTheme('default-2'); // change to own custom theme default-2
+  }
 }
